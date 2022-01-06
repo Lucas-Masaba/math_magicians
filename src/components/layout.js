cfb6c99c2be25d0d93be188c0be2acda5f8c5cc1
Lucas-Masaba <masabaluke@gmail.com>
2022-01-06 19:07:12 +0300

    add events

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,48 +1,71 @@
 import React from 'react';
 import './calculator.css';
+import calculate from '../logic/calculator';
 
 export default class CalculatorLayout extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      zero: '0',
+      obj: {},
+    };
+  }
+
+  calculatorInput = (event) => {
+    const buttonName = event.target.textContent;
+    const { obj } = this.state;
+    this.setState({
+      obj: calculate(obj, buttonName),
+      zero: '',
+    });
+    if (buttonName === 'AC') {
+      this.setState({
+        zero: '0',
+      });
+    }
   }
 
   render() {
+    const { obj } = this.state;
+    const { zero } = this.state;
     return (
       <div className="App">
         <div className="calculator">
           <div className="display">
-            <span>0</span>
+            {zero}
+            {obj.total}
+            {obj.operation}
+            {obj.next}
           </div>
           <div className="buttons">
             <div className="first_row">
-              <button type="button">AC</button>
-              <button type="button">+/-</button>
-              <button type="button">%</button>
-              <button className="operator" type="button">/</button>
+              <button onClick={this.calculatorInput} type="button">AC</button>
+              <button onClick={this.calculatorInput} type="button">+/-</button>
+              <button onClick={this.calculatorInput} type="button">%</button>
+              <button onClick={this.calculatorInput} className="operator" type="button">÷</button>
             </div>
             <div className="second_row">
-              <button type="button">7</button>
-              <button type="button">8</button>
-              <button type="button">9</button>
-              <button className="operator" type="button">x</button>
+              <button onClick={this.calculatorInput} type="button">7</button>
+              <button onClick={this.calculatorInput} type="button">8</button>
+              <button onClick={this.calculatorInput} type="button">9</button>
+              <button onClick={this.calculatorInput} className="operator" type="button">x</button>
             </div>
             <div className="third_row">
-              <button type="button">4</button>
-              <button type="button">5</button>
-              <button type="button">6</button>
-              <button className="operator" type="button">-</button>
+              <button onClick={this.calculatorInput} type="button">4</button>
+              <button onClick={this.calculatorInput} type="button">5</button>
+              <button onClick={this.calculatorInput} type="button">6</button>
+              <button onClick={this.calculatorInput} className="operator" type="button">-</button>
             </div>
             <div className="fourth_row">
-              <button type="button">1</button>
-              <button type="button">2</button>
-              <button type="button">3</button>
-              <button className="operator" type="button">+</button>
+              <button onClick={this.calculatorInput} type="button">1</button>
+              <button onClick={this.calculatorInput} type="button">2</button>
+              <button onClick={this.calculatorInput} type="button">3</button>
+              <button onClick={this.calculatorInput} className="operator" type="button">+</button>
             </div>
             <div className="fifth_row">
-              <button type="button">0</button>
-              <button type="button">.</button>
-              <button className="operator" type="button">=</button>
+              <button onClick={this.calculatorInput} type="button">0</button>
+              <button onClick={this.calculatorInput} type="button">.</button>
+              <button onClick={this.calculatorInput} className="operator" type="button">=</button>
             </div>
           </div>
         </div>
